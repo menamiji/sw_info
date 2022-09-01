c8b1a99d6f7e37fd437eac48a081a7cdcfa401b4
hbkim <menamiji@gmail.com>
2022-09-01 20:19:32 +0900

    test1

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'songwoo middle school information',
+      title: 'songwoo middle school information2',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
